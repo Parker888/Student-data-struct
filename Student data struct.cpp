333bf0804a24695ed56243dfcab70b6c2a01e65c
Parker888 <recorderandy@gmail.com>
2019-02-02 01:59:50 +0200

    Created Student data struct.cpp and added struct
    Created in VS Code

---
+++ Student data struct.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n, i, S;
+    cout << "Number of pupils: ";
+    cin >> n;
+
+    cout << endl;
+
+    struct Pupils {
+        char name[20];
+        char surname[20];
+        int age;
+        float media;
+    } C;
+
+    cout << endl;
+
+    for ( i = 1; i <= n; i++ ) {
+        cout << "Pupil number " << i;
+        cout << endl << endl;
+        cout << "Name: ";
+        cin >> C.name;
+        cout << endl;
+        cout << "Surname: ";
+        cin >> C.surname;
+        cout << endl;
+        cout << "Age: ";
+        cin >> C.age;
+        cout << endl;
+        cout << "Media: ";
+        cin >> C.media;
+
+        cout << endl << endl;
+    }
+
+
+
+
+
+
+
+
+}
