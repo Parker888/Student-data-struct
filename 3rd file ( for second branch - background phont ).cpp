5e459dcf3f8177e64c8264eb964015dcc32d5c74
Parker888 <recorderandy@gmail.com>
2019-02-02 02:26:49 +0200

    Changed phont to blue with 1 more commentary

--- 3rd file ( for second branch - background phont ).cpp
+++ 3rd file ( for second branch - background phont ).cpp
@@ -8,5 +8,6 @@ enum ConsoleColor
 };
 
 // #include <iostream> and other I didn't added, cuz
+// if i will add him to first ( Student data struct. cpp )
 // it will repeat, and I don't know, it can may be
 // an error for program
