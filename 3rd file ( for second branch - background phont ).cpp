bdf486117557af2228dcf6b4225d906776855d0d
Parker888 <recorderandy@gmail.com>
2019-02-02 02:26:00 +0200

    Changed console phont to blue

---
+++ 3rd file ( for second branch - background phont ).cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+enum ConsoleColor
+{
+
+                Blue          = 1,   
+
+};
+
+// #include <iostream> and other I didn't added, cuz
+// it will repeat, and I don't know, it can may be
+// an error for program
