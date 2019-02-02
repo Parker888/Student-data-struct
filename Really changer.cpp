2a9982e2a44a333e2afe9a8cc64aa56b208cca15
Parker888 <recorderandy@gmail.com>
2019-02-02 02:50:21 +0200

    Revert "Revert "Revert "Creating Really changer of color"""
    This reverts commit 16e7a4234308d70fc43e1983fdabfb3354e5ad60.

--- Really changer.cpp
+++
@@ -1,7 +0,0 @@
- system("color c2"); 
-
- // Thisi code are really changes background of the CMD
- // last 3rd brunches was didn't change it, idk why
- // this code is more small and you just
- // need to put him after int main() { 
-     
